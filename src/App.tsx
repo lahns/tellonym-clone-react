9ec8bff46d09868d790c4a776a8f42dd013d1793
meowsync <81372963+lahns@users.noreply.github.com>
2023-03-07 12:07:40 +0100

    dsada
    sadsad

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar';
 function App() {
   return (
+    <>
+    <Navbar></Navbar>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +22,7 @@ function App() {
         </a>
       </header>
     </div>
+  </>
   );
 }
 
