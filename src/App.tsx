aecc91d0f0026c77853bf8072e031bc5deb8794d
Dariusz Zielonka <dariuszzielonka727@gmail.com>
2023-03-08 15:13:04 +0100

    .env server url

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
 import { Question } from './Question';
+import logo from './logo.svg';
 
 function App() {
   return (
