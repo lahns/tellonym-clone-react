598c609d60d8d5dbf9652ebf5965bf15cd4a6b4a
Dariusz Zielonka <dariuszzielonka727@gmail.com>
2023-03-10 20:05:29 +0100

    wouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Route } from 'wouter';
 import './App.css';
 import Navbar from './Navbar';
 import { Question } from './Question';
@@ -7,23 +8,28 @@ function App() {
   return (
     <>
     <Navbar></Navbar>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Question question='Bla bla bla, bla bla bla, bla bla bla, bla bla bla, bla bla bla, bla bla bla, bla bla bla, bla bla bla, bla bla bla, bla bla bla, bla bla bla?' answer='mhm, śmiga ziomek'></Question>
-      </header>
-    </div>
+    <Route path="/">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Question question='Bla bla bla, bla bla bla, bla bla bla, bla bla bla, bla bla bla, bla bla bla, bla bla bla, bla bla bla, bla bla bla, bla bla bla, bla bla bla?' answer='mhm, śmiga ziomek'></Question>
+        </header>
+      </div>
+    </Route>
+    <Route path="/test">
+      <div>test route</div>
+    </Route>
     </>
   );
 }
