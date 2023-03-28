4fa4b0f2bfa44d41138befb80be806d12b929c5e
meowsync <81372963+lahns@users.noreply.github.com>
2023-03-28 11:16:59 +0200

    added routing for navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import search_icon from "./icons/search_icon.svg";
 import home_icon from "./icons/home_icon.svg";
 import account_icon from "./icons/account_icon.svg";
+import { Link  } from "wouter";
 
 export default function Navbar(){
     return(
         <>
         <nav className="flex justify-center items-center gap-16 pt-2 pb-2 bg-white drop-shadow-lg">
-            <img className="scale-10 h-10" src={home_icon}></img>
-            <img className="scale-10 h-10" src={search_icon}></img>
-            <img className="scale-10 h-10" src={account_icon}></img>
+            <Link to="/home"><img className="scale-10 h-10" src={home_icon}></img></Link>
+            <Link to="/search"><img className="scale-10 h-10" src={search_icon}></img></Link>
+            <Link to='/profile/{]'><img className="scale-10 h-10" src={account_icon}></img></Link>
         </nav>
         </>
     );
