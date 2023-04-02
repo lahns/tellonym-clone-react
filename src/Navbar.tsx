1f37a232032983dc660b2ba196a2eb8eae3d2a31
Dariusz Zielonka <dariuszzielonka727@gmail.com>
2023-04-02 18:47:34 +0200

    svg components instead of img

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,16 +1,15 @@
-import React from "react";
-import search_icon from "./icons/search_icon.svg";
-import home_icon from "./icons/home_icon.svg";
-import account_icon from "./icons/account_icon.svg";
-import { Link  } from "wouter";
+import { Link } from "wouter";
+import { ReactComponent as AccountIcon } from "./icons/account_icon.svg";
+import { ReactComponent as HomeIcon } from "./icons/home_icon.svg";
+import { ReactComponent as SearchIcon } from "./icons/search_icon.svg";
 
 export default function Navbar(){
     return(
         <>
         <nav className="flex justify-center items-center gap-16 pt-2 pb-2 bg-white drop-shadow-lg">
-            <Link to="/home"><img className="scale-10 h-10" src={home_icon}></img></Link>
-            <Link to="/search"><img className="scale-10 h-10" src={search_icon}></img></Link>
-            <Link to='/profile/{]'><img className="scale-10 h-10" src={account_icon}></img></Link>
+            <Link to="/home"><HomeIcon className="scale-10 h-10"/></Link>
+            <Link to="/search"><SearchIcon className="scale-10 h-10"/></Link>
+            <Link to='/profile/{]'><AccountIcon className="scale-10 h-10"/></Link>
         </nav>
         </>
     );
