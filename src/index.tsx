1a563acfe21320b3b2b6d3ea64a2d25bf6751418
Dariusz Zielonka <94898544+dariuszzz@users.noreply.github.com>
2023-04-04 11:18:41 +0200

    .

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,9 @@ import Navbar from './Navbar';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
   </React.StrictMode>
 );
 
