18dda0ec17f92827da433674e8cff83252525e8a
Dariusz Zielonka <81372963+lahns@users.noreply.github.com>
2023-05-24 11:42:50 +0200

    displaying full-blown questions on homepage

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { useAppContext } from "./context";
-import { Question, QuestionWithAnswer } from "./types";
+import {QuestionWithAnswer, User } from "./types";
 import { apiFollowers, apiGetUserQuestions } from "./utils/apiUtil";
+import Question from "./Question";
 
 const Home = () => {
   const [qwas, setqwas] = useState<QuestionWithAnswer[] | null>(null);
   const { context } = useAppContext();
+
+  const askerReducer = (
+    { map }: { map: Map<number, User> },
+    { id, userData }: { id: number; userData: User }
+  ) => {
+    map.set(id, userData);
+    return { map };
+  };
+
+  const [{ map: askerMap }, dispatchAskers] = useReducer(askerReducer, {
+    map: new Map(),
+  });
+
+
   useEffect(() => {
     (async () => {
       if (context.currentUser != null) {
@@ -30,16 +45,27 @@ const Home = () => {
   }, [context.currentUser]);
 
   return (
-    <div>
-      {qwas?.length === 0 ? (
-        <div>
-          <h1>Nothing here yet.</h1>
-        </div>
-      ) : (
-        <div>{JSON.stringify(qwas)}</div>
-      )}
-    </div>
-  );
-};
+    <div className="w-full rounded-lg overflow-hidden">
+                {
+                  qwas?.length === 0 ?
+                    <div className="text-gray-outline text-3xl font-bold text-center">
+                      No questions yet
+                    </div>
+                  :
+                  qwas?.map((qwa, index) => (
+                    <Question
+                      key={index}
+                      questionWithAnswer={qwa}
+                      asker={
+                        qwa.question.asker_id
+                          ? askerMap.get(qwa.question.asker_id) ?? null
+                          : null
+                      }
+                    />
+                  ))
+                }
+              </div>
+            )
+  };
 
 export default Home;
