0e34080ada03a61c6a8e88f7c4a47cde1a9ce2f2
Dariusz Zielonka <81372963+lahns@users.noreply.github.com>
2023-05-24 11:31:16 +0200

    rendering questions on homepage in minimal readable form

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,31 +4,42 @@ import { Question, QuestionWithAnswer } from "./types";
 import { apiFollowers, apiGetUserQuestions } from "./utils/apiUtil";
 
 const Home = () => {
-  const [qwas, setqwas] = useState<QuestionWithAnswer[] | null>(null)
+  const [qwas, setqwas] = useState<QuestionWithAnswer[] | null>(null);
   const { context } = useAppContext();
-  useEffect(()=>{
-    if (context.currentUser != null) {
-      let follows: number[] = [];
-      context.following.forEach((user) => {
-        follows.push(user.id);
-      });
-      let questions_of_following : QuestionWithAnswer[] = [];
-      follows.forEach(follow => {
-        if(follow != null){
-          apiGetUserQuestions(follow).then(res => res && questions_of_following.push(...res))
+  useEffect(() => {
+    (async () => {
+      if (context.currentUser != null) {
+        let follows: number[] = [];
+        context.following.forEach((user) => {
+          follows.push(user.id);
+        });
+        let questions_of_following: QuestionWithAnswer[] = [];
+
+        for (const follow of follows) {
+          if (follow != null) {
+            await apiGetUserQuestions(follow).then(
+              (res) => res && questions_of_following.push(...res)
+            );
+          }
         }
-      })
-      if(qwas === null)
-        setqwas(questions_of_following)
-      // questions_of_following.forEach(question =>{
-  
-      // })
-    }
-  })
-  
 
+        if (qwas === null) setqwas(questions_of_following);
+        console.log(qwas);
+      }
+    })();
+  }, [context.currentUser]);
 
-  return <div>`{JSON.stringify(qwas)}`</div>;
+  return (
+    <div>
+      {qwas?.length === 0 ? (
+        <div>
+          <h1>Nothing here yet.</h1>
+        </div>
+      ) : (
+        <div>{JSON.stringify(qwas)}</div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
