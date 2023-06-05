036f34f7d10a88368e2e07f6b37520a44b0d1882
lahns <megaice@interia.pl>
2023-06-05 20:16:39 +0200

    liking util full
    adds and removes(if a like is already there) likes from context

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -45,16 +45,23 @@ export const fetchAskerData = async (questions: QuestionWithAnswer[]): Promise<M
 export function likeResource(questionWithAnswer : QuestionWithAnswer, likeType : Like["like_type"], {context, setContext}: ReturnType<typeof useAppContext>){
   if (context.currentUser) {
 
-    let like_or_dislike = likeType == "QuestionLike" || likeType == "AnswerLike" ? true : false;
+    const like_or_dislike = likeType == "QuestionLike" || likeType == "AnswerLike" ? true : false;
 
-    if(likeType == "QuestionLike" || likeType=="QuestionDislike"){
+    if(likeType == "QuestionLike" || likeType == "QuestionDislike"){
       like_question(questionWithAnswer, like_or_dislike, {context, setContext});
+
+      let Likes = context.currentUser.likes;
+      let answer_likes = Likes.filter(like => like.like_type == "AnswerLike" || like.like_type =="AnswerDislike");
+      let question_likes = Likes.filter(like => like.resource_id != questionWithAnswer.question.id && (like.like_type == "QuestionLike" || like.like_type == "QuestionDislike"));
+
+      let new_likes = [...answer_likes, ...question_likes]
+
       setContext({
         ...context,
         currentUser: {
           ...context.currentUser,
           likes: [
-            ...context.currentUser.likes,
+            ...new_likes,
             {
               like_type: likeType,
               liker_id: context.currentUser.user.id,
@@ -65,13 +72,19 @@ export function likeResource(questionWithAnswer : QuestionWithAnswer, likeType :
       });
     }
     else{
-      like_answer(questionWithAnswer, like_or_dislike, {context, setContext});
+
+      let Likes = context.currentUser.likes;
+      let answer_likes = Likes.filter(like => like.like_type == "QuestionLike" || like.like_type =="QuestionDislike");
+      let question_likes = Likes.filter(like => like.resource_id != questionWithAnswer.answer!.id && (like.like_type == "AnswerLike" || like.like_type == "AnswerDislike"));
+
+      let new_likes = [...answer_likes, ...question_likes]
+
       setContext({
         ...context,
         currentUser: {
           ...context.currentUser,
           likes: [
-            ...context.currentUser.likes,
+            ...new_likes,
             {
               like_type: likeType,
               liker_id: context.currentUser.user.id,
@@ -81,9 +94,5 @@ export function likeResource(questionWithAnswer : QuestionWithAnswer, likeType :
         },
       });
     }
-    }
-
-      
-         
   }
 }
